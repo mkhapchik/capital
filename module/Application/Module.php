74c0d49d0c0a236554616d3e4384e43fa1b3997c
mkhapchik <mkhapchik@gmail.com>
2015-06-30 17:10:34 +0300

    init for prodaction

--- module/Application/Module.php
+++ module/Application/Module.php
@@ -86,13 +86,13 @@ class Module
 	public function checkAccess(MvcEvent $e)
 	{
 		$route = $e->getRouteMatch();
-		\Zend\Debug\Debug::dump($route, '123');
+		//\Zend\Debug\Debug::dump($route, '123');
 		
 	}
 	
 	public function initAcl(MvcEvent $e)
 	{
-		echo __CLASS__;
+		//echo __CLASS__;
 	}
 	
 }
