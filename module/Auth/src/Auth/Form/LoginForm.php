30fe233196fa9f2b5a9a15cd3ddb2c060520a272
mkhapchik <mkhapchik@gmail.com>
2015-09-03 11:07:00 +0300

    убраны комментарии

--- module/Auth/src/Auth/Form/LoginForm.php
+++ module/Auth/src/Auth/Form/LoginForm.php
@@ -1,88 +1,17 @@
 <?php
 namespace Auth\Form;
  
-//use Zend\Form\Form;
 use Application\Form\CaptchaForm;
 use Zend\InputFilter\Factory as InputFactory;
 use Zend\InputFilter\InputFilter;
 use Zend\Validator\StringLength;
 use Zend\Validator\NotEmpty;
 
- 
-//class LoginForm extends Form
 class LoginForm extends CaptchaForm
 {
-    /*
-	public function __construct($name = null)
-    {
-        parent::__construct($name);
-        $this->setAttribute('method', 'post');
-       
-		$this->add(array(
-            'name' => 'login',
-            'attributes' => array(
-                'type'  => 'text',
-            ),
-            'options' => array(
-                'label' => 'Логин',
-            ),
-        ));
-		
-		$this->add(array(
-            'name' => 'pwd',
-            'attributes' => array(
-                'type'  => 'password',
-            ),
-			'options' => array(
-                'label' => 'Пароль',
-            ),
-        ));
-		
-		$this->add(array(
-			'type' => 'Captcha',
-			'name' => 'captcha',
-			'options' => array(
-				'label' => 'Введите код с картинки',
-				'captcha' => array(
-                    //'class' => 'Dumb',
-					'class' => 'Image',
-					'font'=>'./data/captcha/font/arial.ttf',
-					'imgUrl'=>'/img/captcha/',
-					'imgDir'=>'public/img/captcha/',
-					//'gcFreq'=>'1',
-                    //'expiration'=>'1',
-                    'wordlen'=>'5',
-                    //'timeout'=>'1',
-                    'keepSession'=>false,
-					'width'=>120,
-					'height' => 50,
-					'fontSize'=>20,
-					'dotNoiseLevel' => 80,
-					//'lineNoiseLevel' => 0
-					//'class'=>'Figlet'
-					'messages' => array(
-						\Zend\Captcha\AbstractWord::BAD_CAPTCHA => "",
-						\Zend\Captcha\AbstractWord::MISSING_VALUE => "",
-						\Zend\Captcha\AbstractWord::MISSING_ID => "",
-					),
-                ),
-			)
-		));
-		
-		$this->add(array(
-            'name' => 'submit',
-            'attributes' => array(
-                'type'  => 'submit',
-                'value' => 'Войти',
-            ),
-        ));
-		
-		$inputFilter = $this->__getInputFilter();
-		$this->setInputFilter($inputFilter);
-    }
-	*/
 	protected function initForm()
 	{
+		//$this->setUseInputFilterDefaults(false); // отключение стандартных валидаторов
 		$this->setAttribute('method', 'post');
        
 		$this->add(array(
@@ -92,6 +21,7 @@ class LoginForm extends CaptchaForm
             ),
             'options' => array(
                 'label' => 'Логин',
+				
             ),
 			
         ));
@@ -123,10 +53,10 @@ class LoginForm extends CaptchaForm
 			),
 			'validators' => array(
 				array(
-                      'name' =>'NotEmpty', 
+						'name' =>'NotEmpty', 
                         'options' => array(
-                            'messages' => array(
-                               //NotEmpty::IS_EMPTY => 'Поле обязательно для заполнения' 
+                           	'messages' => array(
+                               NotEmpty::IS_EMPTY => '' 
                             ),
                         ),
 				),
@@ -152,7 +82,7 @@ class LoginForm extends CaptchaForm
                       'name' =>'NotEmpty', 
                         'options' => array(
                             'messages' => array(
-                               //NotEmpty::IS_EMPTY => 'Поле обязательно для заполнения' 
+                               NotEmpty::IS_EMPTY => '' 
                             ),
                         ),
 				),
@@ -162,7 +92,7 @@ class LoginForm extends CaptchaForm
 						'encoding' => 'UTF-8',
 						'max'      => 50,
 						'messages' => array(
-							//StringLength::TOO_LONG => 'Длина пароля не должна превышать %max% символов'
+							StringLength::TOO_LONG => 'Длина пароля не должна превышать %max% символов'
 						)
 					),
 				),
