afcda410ca9a4ca28001f85d403679f98705dd16
mkhapchik <mkhapchik@gmail.com>
2015-09-24 16:12:43 +0300

    SendMailService

--- module/Application/src/Application/Service/SendMailService.php
+++ module/Application/src/Application/Service/SendMailService.php
@@ -4,10 +4,24 @@ namespace Application\Service;
 use Zend\Mail\Message;
 use Zend\Mail\Transport\Smtp as SmtpTransport;
 use Zend\Mail\Transport\SmtpOptions;
+use Zend\Mime\Message as MimeMessage;
+use Zend\Mime\Part as MimePart;
+use Zend\Mime\Mime as Mime;
 
 class SendMailService
 {
+	const TYPE_INTERNAL = 'INTERNAL';
+	const TYPE_EXTERNAL = 'EXTERNAL';
+	
 	protected $options;
+	protected $transport;
+	
+	/**
+	 *  Тип отправки сообщений
+	 *  1 - php mail
+	 *  0 - smtp
+	 */
+	protected $type;
 	/**
 	*  send email
 	*  @param ARRAY $options
@@ -17,13 +31,12 @@ class SendMailService
 				'pswd'=>SMTP_PSWD,
 				'port'=>SMTP_PORT,
 			),
-			'message'=>array(
+			'messages'=>array(array(
 				'to'=>email to,
 				'from'=>from,
 				'subject'=>'subject';
-				'encoding'=>'utf-8',
 				'body'=>"text"
-			)
+			))
 	
 	*/
 	public function setOptions($options)
@@ -57,27 +70,73 @@ class SendMailService
 	{
 		$options = $this->getOptions();
 		
+		$this->init();
+		
+		foreach($options['messages'] as $m)
+		{
+			$this->sendMessage($m);
+		}
+	}
+	
+	public function init()
+	{
+		$options = $this->getOptions();
+		$this->type = $options['type'];
+		
+		$this->transport = new SmtpTransport();
+		
+		if($this->type == self::TYPE_EXTERNAL)
+		{
+			$smtp_options   = new SmtpOptions(array(
+				'host'              => $options['smtp']['server'],
+				'connection_class'  => 'login',
+				'connection_config' => array(
+					'username' => $options['smtp']['login'],
+					'password' => $options['smtp']['pswd'],
+				),
+				'port'=>$options['smtp']['port']
+			));
+			$this->transport->setOptions($smtp_options);
+		}
+	}
+	
+	/**
+	 *  @param ARRAY $m = array(
+	 *  	'to'
+	 *  	'from'
+	 *  	'subject'
+	 *  	'body'
+	 *  	'encoding'
+	 *  )
+	 */
+	public function sendMessage($m)
+	{
 		$message = new Message();
-		$message->addTo($options['message']['to'])
-				->addFrom($options['smtp']['login'])
-				->setSubject($options['message']['subject'])
-				->setBody($options['message']['body'])
-				->setEncoding($options['message']['encoding']);
-
-		$transport = new SmtpTransport();
-		$smtp_options   = new SmtpOptions(array(
-			'host'              => $options['smtp']['server'],
-			'connection_class'  => 'login',
-			'connection_config' => array(
-				'username' => $options['smtp']['login'],
-				'password' => $options['smtp']['pswd'],
-			),
-			'port'=>$options['smtp']['port']
-		));
-		$transport->setOptions($smtp_options);
 		
+		if(is_array($m['to'])) foreach($m['to'] as $email) $message->addTo($email);
+		else $message->addTo($m['to']);
+	
+		$message->addFrom($m['from']);
+		$message->setSubject($m['subject']);
 		
-		$transport->send($message);
+		$body = $this->getBody($m['body']);
+		$message->setBody($body);
+		
+		$message->setEncoding('utf-8');
+		
+		$this->transport->send($message);
+	}
+	
+	protected function getBody($text)
+	{
+		$text = new MimePart($text);
+		$text->type = Mime::TYPE_TEXT;
+		$text->encoding = Mime::ENCODING_QUOTEDPRINTABLE;
+		$text->charset = 'utf-8';
+		$body = new MimeMessage();
+		$body->setParts(array($text));
+		
+		return $body;
 	}
 }
 
